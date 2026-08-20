f8bc1ebda13de3610bc2fbc4699b3334de441fd0
delta030737-pixel <delta030737@gmail.com>
2026-08-20 15:33:57 +0700

    Bug fix and Reset Position

--- Assets/Scripts/Bowling.cs
+++ Assets/Scripts/Bowling.cs
@@ -1,42 +1,82 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using System.Collections.Generic;
+using TMPro;
+
 public class Bowling : MonoBehaviour
 {
-    [SerializeField]
-    private Rigidbody rb;
+    public Rigidbody rb;
+    [SerializeField] private int forcePower;
+    [SerializeField] private float resetZPosition = 20f;
 
-    [SerializeField]
-    private int forcePower;
+    [Header("UI Settings")]
+    public GameObject resetTextObject;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Vector3 ballStartPosition;
+    private bool isShot = false;
+
+    private List<Transform> pinTransforms = new List<Transform>();
+    private List<Vector3> pinStartPositions = new List<Vector3>();
+    private List<Quaternion> pinStartRotations = new List<Quaternion>();
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        ballStartPosition = transform.position;
+
+        if (resetTextObject != null)
+        {
+            resetTextObject.SetActive(false);
+        }
+
+        GameObject[] pins = GameObject.FindGameObjectsWithTag("Pin");
+        foreach (GameObject pin in pins)
+        {
+            pinTransforms.Add(pin.transform);
+            pinStartPositions.Add(pin.transform.position);
+            pinStartRotations.Add(pin.transform.rotation);
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (Keyboard.current.spaceKey.wasPressedThisFrame
-            || Mouse.current.leftButton.wasPressedThisFrame)
+        if (!isShot && (Keyboard.current.spaceKey.wasPressedThisFrame || Mouse.current.leftButton.wasPressedThisFrame))
         {
             ShootBall();
         }
-        if (Keyboard.current.rightArrowKey.isPressed 
-            || Keyboard.current.dKey.isPressed)
+
+        if (Keyboard.current.rKey.wasPressedThisFrame)
         {
-            MoveRight();
+            ResetGame();
         }
-        if (Keyboard.current.leftArrowKey.isPressed
-            || Keyboard.current.aKey.isPressed)
+
+        if (!isShot)
         {
-             MoveLeft();
-        }    
+            if (Keyboard.current.rightArrowKey.isPressed || Keyboard.current.dKey.isPressed)
+            {
+                MoveRight();
+            }
+            if (Keyboard.current.leftArrowKey.isPressed || Keyboard.current.aKey.isPressed)
+            {
+                MoveLeft();
+            }
+        }
     }
 
     private void ShootBall()
     {
+        isShot = true;
         rb.AddForce(Vector3.forward * forcePower, ForceMode.Impulse);
+
+        Invoke("ShowResetText", 1.0f);
+    }
+
+    private void ShowResetText()
+    {
+        if (resetTextObject != null)
+        {
+            resetTextObject.SetActive(true);
+        }
     }
 
     private void MoveRight()
@@ -47,5 +87,31 @@ public class Bowling : MonoBehaviour
     private void MoveLeft()
     {
         transform.position += new Vector3(-0.5f, 0f, 0f) * Time.deltaTime;
+    }
+
+    private void ResetGame()
+    {
+        isShot = false;
+
+        rb.linearVelocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+        transform.position = ballStartPosition;
+
+        for (int i = 0; i < pinTransforms.Count; i++)
+        {
+            Rigidbody pinRb = pinTransforms[i].GetComponent<Rigidbody>();
+            if (pinRb != null)
+            {
+                pinRb.linearVelocity = Vector3.zero;
+                pinRb.angularVelocity = Vector3.zero;
+            }
+            pinTransforms[i].position = pinStartPositions[i];
+            pinTransforms[i].rotation = pinStartRotations[i];
+        }
+
+        if (resetTextObject != null)
+        {
+            resetTextObject.SetActive(false);
+        }
     }
 }
